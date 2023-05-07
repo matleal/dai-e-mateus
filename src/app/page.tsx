6a821903b75fc7db9a1fae09dde401806e42d0e3
matleal <matcod2@gmail.com>
2023-05-07 16:22:48 -0300

    Added favicon and the title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
+import Head from "next/head";
 import Cronometro from "./components/Cronometro";
 import Navbar from "./components/Navbar";
 import SaveTheDate from "./components/SaveTheDate";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-6 min-h-screen">
-      <Navbar></Navbar>
-      <SaveTheDate></SaveTheDate>
-      <Cronometro></Cronometro>
-    </div>
+    <>
+      <Head>
+        <title>Dai e Ted</title>
+      </Head>
+      <div className="flex flex-col gap-6 min-h-screen">
+        <Navbar></Navbar>
+        <SaveTheDate></SaveTheDate>
+        <Cronometro></Cronometro>
+      </div>
+    </>
   );
 }
