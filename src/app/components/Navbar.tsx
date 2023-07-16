205c90282ae2a7cf7ced328b81a44b0550ed0f84
matzleal <mateus.leal@growinco.com>
2023-07-16 15:42:12 -0300

    Added location, history, padrinhos and madrinhas section

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,22 +1,41 @@
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-center bg-white px-10 py-6 border-b border-2 border-grey-300">
+    <nav className="flex items-center justify-between bg-white px-10 py-6 border-b border-2 border-grey-300">
       <div className="flex items-center">
         <span
-          className="text-gray-500 font-bold text-lg"
+          className="text-gray-500 font-bold text-lg cursor-pointer"
           style={{ fontFamily: "brittany" }}
         >
           Daillayne e Mateus
         </span>
       </div>
-      {/* <ul className="flex items-center">
+      <ul className="flex items-center text-sm">
         <li className="ml-6">
-          <a className="text-gray-500 hover:text-gray-200">Save the date</a>
+          <a className="text-gray-500 hover:text-gray-200 cursor-pointer">
+            Historia
+          </a>
         </li>
         <li className="ml-6">
-          <a className="text-gray-500 hover:text-gray-200">Contador</a>
+          <a className="text-gray-500 hover:text-gray-200 cursor-pointer">
+            Onde e quando
+          </a>
         </li>
-      </ul> */}
+        <li className="ml-6">
+          <a className="text-gray-500 hover:text-gray-200 cursor-pointer">
+            Padrinhos e Madrinhas
+          </a>
+        </li>
+        <li className="ml-6">
+          <a className="text-gray-500 hover:text-gray-200 cursor-pointer">
+            Presentes
+          </a>
+        </li>
+        <li className="ml-6">
+          <a className="text-gray-500 hover:text-gray-200 cursor-pointer">
+            Confirmação
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
