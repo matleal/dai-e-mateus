6a9d1880cc1aa2a07236b09ac421f609f481adbd
matleal <matcod2@gmail.com>
2023-08-26 23:25:25 -0300

    Changed mercado pago acess token to prod

--- src/app/api/mercado-pago/reference/route.ts
+++ src/app/api/mercado-pago/reference/route.ts
@@ -2,8 +2,7 @@ import { getBaseUrl } from "@/app/services/url";
 import mercadopago from "mercadopago";
 
 mercadopago.configure({
-  access_token:
-    "TEST-7392384281194045-081710-716f0f342027f6e34a7cfa04d2ae9d0d-181959770",
+  access_token: process.env.MERCADO_PAGO_ACCESS_TOKEN!,
 });
 
 export async function POST(req: Request) {
@@ -18,6 +17,11 @@ export async function POST(req: Request) {
           quantity: 1,
         },
       ],
+      payment_methods: {
+        excluded_payment_methods: [],
+        excluded_payment_types: [{ id: "ticket" }],
+        installments: 12,
+      },
       payer: {
         email: body.payerEmail,
         name: body.payerName,
