a740d0b85367db5bd2fe818848ba01b94e8cda39
matzleal <mateus.leal@growinco.com>
2023-10-08 12:11:59 -0300

    Update

--- src/app/pages/guests/page.tsx
+++ src/app/pages/guests/page.tsx
@@ -22,7 +22,7 @@ export default async function Page() {
     .sort((a: any, b: any) => (a.data.toDate() > b.data.toDate() ? -1 : 1))
     .map((guest: any) => (
       <tr key={guest.id}>
-        <td className="px-6 py-4 whitespace-nowrap">
+        <td className="whitespace-nowrap px-6 py-4">
           <div className="flex items-center">
             <div className="ml-4">
               <div className="text-sm font-medium text-gray-900">
