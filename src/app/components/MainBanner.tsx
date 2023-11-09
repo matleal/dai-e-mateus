64cd9d331fc144dc231dec9234c7dd82dd8905c5
matleal <matcod2@gmail.com>
2023-11-08 23:15:01 -0300

    Changed the photos from main banner

--- src/app/components/MainBanner.tsx
+++ src/app/components/MainBanner.tsx
@@ -4,22 +4,22 @@ export default function MainBanner() {
       <img
         loading="lazy"
         className="aspect-w-9 aspect-h-16 flex-grow flex-shrink w-[15px] object-cover opacity-50 transition-all duration-500 ease-in-out hover:opacity-100 hover:w-[155px]"
-        src="/assets/_MG_9539.jpg"
+        src="/assets/photo-one.jpg"
       />
       <img
         loading="lazy"
         className="aspect-w-9 aspect-h-16 flex-grow flex-shrink w-[15px] object-cover opacity-50 transition-all duration-500 ease-in-out hover:opacity-100 hover:w-[155px]"
-        src="/assets/_MG_9583.jpg"
+        src="/assets/photo-two.jpg"
       />
       <img
         loading="lazy"
         className="aspect-w-9 aspect-h-16 flex-grow flex-shrink w-[15px] object-cover opacity-50 transition-all duration-500 ease-in-out hover:opacity-100 hover:w-[155px]"
-        src="/assets/_MG_9652.jpg"
+        src="/assets/photo-three.jpg"
       />
       <img
         loading="lazy"
         className="hidden md:block aspect-w-9 aspect-h-16 flex-grow flex-shrink w-[15px] object-cover opacity-50 transition-all duration-500 ease-in-out hover:opacity-100 hover:w-[155px]"
-        src="/assets/_MG_9548.jpg"
+        src="/assets/photo-four.jpg"
       />
     </div>
   );
